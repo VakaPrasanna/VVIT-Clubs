6e433453c7f1ae23c076ecb6b3c9cbac1a9128c1
VakaPrasanna <20BQ1A05O6@vvit.net>
2023-07-23 09:32:51 +0530

    responsive commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const menubutton=document.querySelector(".menu");
-menubutton.addEventListener("click", ()=>{
-    menubutton.classList.toggle("active");
+document.addEventListener('DOMContentLoaded', function(){
+    const menubutton=document.querySelector(".menu");
+    if(menubutton){
+        menubutton.addEventListener('click', function(){
+            menubutton.classList.toggle("active");
+        });
     }
-);
+});
